3e32e913b36d669cb033250fa61c83c37bc29cda
Petra <pasztor.petra1998@gmail.com>
2022-08-19 15:59:16 +0200

    final version

--- 01/solution/js/summarize.js
+++ 01/solution/js/summarize.js
@@ -1,5 +1,11 @@
-const summarize = (...numbers) => {
-  if 
-};
+function summarize(...numbers) {
+  let intNumbers = numbers.filter((item) => Number.isInteger(item));
+  intNumbers = intNumbers.map((item) => BigInt(item));
+  const result = intNumbers.reduce((a, b) => a + b);
+
+  return result > Number.MAX_SAFE_INTEGER || result < Number.MIN_SAFE_INTEGER
+    ? result
+    : Number(result);
+}
 
 export default summarize;
